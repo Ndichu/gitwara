8713d7fe9dec0c59548d35bb6d5b1a596db3c064
Ndichu <georgemiriendichu@gmail.com>
2022-04-29 18:42:07 +0300

    heightupdate6

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ ScreenManager:
         
         MDCard:
             size_hint: None, None
-            size:root.width,1390
+            size:root.width,root.height
             spacing: 25
             padding: 25
             elevation: 12
